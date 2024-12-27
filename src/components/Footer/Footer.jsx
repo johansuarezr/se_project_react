c50c975bc819df556284d5388ef38326ebd99c1e
johansuarezr <johansuarez@Johans-MacBook-Air.local>
2024-12-27 12:15:36 -0500

    setup fonts

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>Developed by Johan Suarez</div>
+      <div>2024</div>
+    </footer>
+  );
+}
+
+export default Footer;
