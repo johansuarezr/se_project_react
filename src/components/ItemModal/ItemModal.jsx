e2af6d1ee90bfd07c098b8cd2494ec056dbbf05d
johansuarezr <johansuarez@Johans-MacBook-Air.local>
2024-12-28 12:02:54 -0500

    fix modals

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,11 +1,12 @@
 import "./ItemModal.css";
+import closeButton from "../../assets/closeButton.png";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
-          Close
+          <img src={closeButton} alt="closeButton" />
         </button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,5 @@
 import "./ModalWithForm.css";
+import closeButton from "../../assets/closeButton.png";
 
 function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
@@ -8,7 +9,7 @@ function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} type="button" className="modal__close">
-          Close
+          <img src={closeButton} alt="closeButton" />
         </button>
         <form className="modal__form">
           {children}
